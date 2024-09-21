ca07ddaef8131867f70367274df22c0dc1485fb7
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-09-21 19:19:49 +0800

    Fix deploy.sh

--- pkg/driver/deploy.sh
+++ pkg/driver/deploy.sh
@@ -29,12 +29,13 @@ export FUNC_NAME=$2
 
 export CPU_REQUEST=$3
 export CPU_LIMITS=$4
-INIT_SCALE=$5
+export MEMORY_REQUESTS=$5
+INIT_SCALE=$6
 
-export PANIC_WINDOW=$6
-export PANIC_THRESHOLD=$7
+export PANIC_WINDOW=$7
+export PANIC_THRESHOLD=$8
 
-export AUTOSCALING_METRIC=$8
-export AUTOSCALING_TARGET=${9}
+export AUTOSCALING_METRIC=$9
+export AUTOSCALING_TARGET=${10}
 
 cat $CONFIG_FILE | envsubst | kn service apply $FUNC_NAME --scale-init $INIT_SCALE --concurrency-target 1 --wait-timeout 2000000 -f /dev/stdin
