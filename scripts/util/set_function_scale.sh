2293bda396ed82ccd49335eb786a36801c2db8f6
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-12-31 21:11:41 +0530

    Updates

--- scripts/util/set_function_scale.sh
+++ scripts/util/set_function_scale.sh
@@ -25,11 +25,12 @@
 
 SCALE=$1
 
-bash ./pkg/function/deploy.sh \
+bash ./pkg/driver/deploy.sh \
     workloads/container/trace_func_go.yaml \
     myfunc \
-    100Gi \
     1000m \
+    1000m \
+    100Gi \
     $SCALE \
     '"10.0"' \
     '"200.0"'
