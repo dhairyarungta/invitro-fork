66eef65d117052b2dee0aed71bea2473802795ea
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2025-01-27 20:44:31 +0800

    Experiment settings

--- pkg/trace/profiler.go
+++ pkg/trace/profiler.go
@@ -47,6 +47,8 @@ func ApplyResourceLimits(functions []*common.Function, CPULimit string) {
 		switch CPULimit {
 		case "1vCPU":
 			cpuShare = 1000
+		case "2vCPU":
+			cpuShare = 2000
 		case "GCP":
 			cpuShare = ConvertMemoryToCpu(memoryPct100)
 		}
